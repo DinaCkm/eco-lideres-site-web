cfec40a67bf15efa0e37b0df364d3aafef1011bb
Financeiro <dina@ckmtalents.net>
2026-07-21 18:35:32 -0300

    Padroniza menu do blog com o site institucional

--- src/routes/blog.js
+++ src/routes/blog.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const router = express.Router();
+const { transformSiteHtml } = require('../site-transform');
+
+function sendBlogFile(fileName) {
+  return (req, res, next) => {
+    const filePath = path.join(__dirname, '../../public', fileName);
+
+    fs.readFile(filePath, 'utf8', (err, html) => {
+      if (err) return next(err);
+      res.type('html').send(transformSiteHtml(html));
+    });
+  };
+}
 
 // /blog → listagem
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/blog.html'));
-});
+router.get('/', sendBlogFile('blog.html'));
 
 // /blog/:slug → post individual
-router.get('/:slug', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/blog-post.html'));
-});
+router.get('/:slug', sendBlogFile('blog-post.html'));
 
 module.exports = router;
